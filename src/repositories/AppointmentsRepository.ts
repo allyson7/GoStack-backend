316f0e47a4c0cd0042266566f4c8ff5e1bac68de
Allysonlopes <allyson@m2smart.com.br>
2021-02-23 18:15:44 -0400

    fourth commit

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,13 +1,13 @@
-import { isEqual } from 'date-fns';
-import Appointment from '../models/Appointment';
 import { EntityRepository, Repository } from 'typeorm';
 
+import Appointment from '../models/Appointment';
+
 @EntityRepository(Appointment)
-class AppointmentsRepository extends {
-  public findByDate(date: Date): Appointment | null {
-    const findAppointment = this.appointments.find(appointment =>
-      isEqual(date, appointment.date),
-    );
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({
+      where: { date },
+    });
 
     return findAppointment || null;
   }
